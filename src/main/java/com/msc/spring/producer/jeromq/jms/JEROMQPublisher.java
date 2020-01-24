60a584d7ddd8e8ef1f469b16253647a7a5e50616
Anna Dowling <annadowling@annas-mbp-2.home>
2020-01-24 22:35:43 +0000

    work on jeromq testing

--- src/main/java/com/msc/spring/producer/jeromq/jms/JEROMQPublisher.java
+++ src/main/java/com/msc/spring/producer/jeromq/jms/JEROMQPublisher.java
@@ -32,25 +32,30 @@ public class JEROMQPublisher {
     @Value("${jeromq.enabled}")
     private boolean jeroMQEnabled;
 
+    final String errorMessage = "Exception encountered = ";
 
     @Bean
     public void setUpJMQProducerAndSendMessage() {
         if (jeroMQEnabled) {
-            ZMQ.Context ctx = ZMQ.context(1);
+            try (ZMQ.Context context = ZMQ.context(1);
+                 ZMQ.Socket publisher = context.socket(ZMQ.PUB);
+            ) {
+                publisher.bind(bindAddress);
+                System.out.println("Starting Publisher..");
+                publisher.setIdentity("B".getBytes());
+                // for testing setting sleep at 100ms to ensure started.
+                Thread.sleep(100l);
+                for (int i = 1; i <= 10; i++) {
+                    publisher.sendMore("B");
+                    boolean isSent = publisher.send("X(" + System.currentTimeMillis() + "):" + i);
+                    System.out.println("Message was sent " + i + " , " + isSent);
+                }
+                publisher.close();
+                context.term();
+            } catch (Exception e) {
+                System.out.println(errorMessage + e.getLocalizedMessage());
 
-            ZMQ.Socket publisher = ctx.socket(ZMQ.PUB);
-            publisher.bind(bindAddress);
-
-            Message message = new Message();
-
-            int i = 0;
-            while (i < messageVolume) {
-                System.out.println("Sending Message = " + message.toString());
-                publisher.send(message.toString());
-                i++;
             }
-            publisher.close();
-            ctx.close();
         }
     }
 }
