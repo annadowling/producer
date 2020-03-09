de4ddaff8d7c267ceeff5d53b5bccd159b72ec6c
annadowling <anna.dowling@errigal.com>
2020-03-09 12:58:34 +0000

    Added more GC's

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UseG1GC -Djava.library.path=/usr/local/lib"
+#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+USeParNewGC -Djava.library.path=/usr/local/lib"
+#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UseSerialGC -Djava.library.path=/usr/local/lib"
+#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UseParallelGC -Djava.library.path=/usr/local/lib"
 JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC"
 export JAVA_OPTS
 
